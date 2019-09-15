328167b7d1e1fca36b032ee5ba24286656e82f18
Jessi Calicotte <orgkleen@gmail.com>
2019-09-15 19:47:04 +0000

    fixing errors and styling

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,10 +6,11 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
- # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
- RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+  include ActionDispatch::TestProcess
+end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -23,26 +24,21 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
-
+# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+​
 # Checks for pending migrations and applies them before tests are run.
-# If you are not using ActiveRecord, you can remove these lines.
+# If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
-
+​
 RSpec.configure do |config|
-  config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include Devise::Test::ControllerHelpers, type: :view
-  include ActionDispatch::TestProcess
-end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+​
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
+​
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
@@ -57,7 +53,7 @@ end
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
-
+​
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
