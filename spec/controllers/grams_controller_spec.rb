7cee7f500485ac4d8bd9ba944959943dd8e94fe2
Jessi Calicotte <orgkleen@gmail.com>
2019-09-15 18:46:05 +0000

    fixing errors

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -178,7 +178,7 @@ RSpec.describe GramsController, type: :controller do
         it "should return http status code of not found if the gram isn't found" do
             user = FactoryBot.create(:user)
             sign_in user
-            post :create, params: { gram_id: 'YOLOSWAG', comment: { message: 'awesome gram' }
+            post :create, params: { gram_id: 'YOLOSWAG', comment: { message: 'awesome gram' } }
             expect(response).to have_http_status :not_found
         end
     end 
